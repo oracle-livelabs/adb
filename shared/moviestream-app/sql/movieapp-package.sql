136a5e999f73dbeb75b0f3a62f702c6e43c913d1
Marty <martygubar@users.noreply.github.com>
2022-11-09 11:20:42 -0500

    movieapp updates.  (#94)
    movieapp updates.

--- shared/moviestream-app/sql/movieapp-package.sql
+++ shared/moviestream-app/sql/movieapp-package.sql
@@ -1,3 +1,18 @@
+create or replace procedure moviestream_write 
+(
+  message in varchar2 default ''
+) as 
+begin
+    dbms_output.put_line(to_char(systimestamp, 'DD-MON-YY HH:MI:SS') || ' - ' || message); 
+    
+    if message is not null then
+        execute immediate 'insert into moviestream_log values(:t1, :msg)' 
+                using systimestamp, message;
+        commit;
+    end if;
+
+end moviestream_write;
+/
 create or replace package movieapp as 
 
 
