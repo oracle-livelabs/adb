42a1401c40c8402b272d21dcb56a2e8bc6e86138
Marty <martygubar@users.noreply.github.com>
2023-03-17 09:11:53 -0400

    These changes are to the MovieStream application - which has not yet been released as a workshop (#146)
    * movieapp updates.
    
    * Movieapp install updates
    
    * Install file updates
    
    * install updates
    
    * install updates
    
    * Edits due to UI changes. Fix to free tier manifest
    
    * fix based on lintchecker

--- shared/moviestream-app/sql/movieapp-add-data.sql
+++ shared/moviestream-app/sql/movieapp-add-data.sql
@@ -1,5 +1,5 @@
 /*
-0. Create a moviestream user
+
 1. Start with base moviestream
 2. Update movie and movie similarity to add ratings TODO: Update movie similarity
 3. Add movie_recommendations table.  Will be populated inititially with random data
@@ -12,46 +12,171 @@
 
 */
 
--- Add the log table
-create table moviestream_log
-   (    execution_time timestamp (6),
-        message varchar2(32000 byte)
-   );
 
 -- Install base moviestream
 -- Install the setup file from github 
-    declare
-        l_git varchar2(4000);
-        l_repo_name varchar2(100) := 'common';
-        l_owner varchar2(100) := 'oracle-livelabs';
-        l_package_file varchar2(200) := 'building-blocks/setup/workshop-setup.sql';
-    begin
-        -- get a handle to github
-        l_git := dbms_cloud_repo.init_github_repo(
-                    repo_name       => l_repo_name,
-                    owner           => l_owner );
 
-        -- install the package header
-        dbms_cloud_repo.install_file(
-            repo        => l_git,
-            file_path   => l_package_file,
-            stop_on_error => false);
+-- CONNECT As MOVIESTREAM user
+begin
+    --
+    -- Add/modify data required for the demo
+    --
+    -- Customers used by the demo.  Update this table if nec.   
 
-    end;
-    /
-
--- Run the PLSQL procedure that loads the rest of the dataset
-BEGIN
     workshop.add_dataset('ALL');
-END;
+    workshop.write('Add/modify demo data', 1);
+
+    -- Drop demo tables if they exist
+    for t in (
+        select table_name
+        from user_tables
+        where table_name in (
+            'DEMO_CUSTOMERS',
+            'LEARN_MORE',
+            'NOTIFICATIONS',
+            'EXT_MOVIE',
+            'MOVIE',
+            'EXT_MOVIE_SIMILARITY',
+            'MOVIE_SIMILARITY',
+            'EXT_BROKEN_MOVIES',
+            'MOVIE_RECOMMENDATIONS' )
+        )
+    loop
+        workshop.write('dropping ' || t.table_name);
+        workshop.exec('drop table ' || t.table_name || ' cascade constraints');
+    end loop;
+    
+    workshop.write('Create tables',1);
+
+end;
 /
 
--- Customers used by the demo.  Update this table if nec.   
-drop table demo_customers;   
+
+-- Customers on the top screen
 create table demo_customers (
- cust_id number,
- img_url varchar2(1000)
- );
+     cust_id number,
+     img_url varchar2(1000)
+     );
+
+-- Pointers to tutorials, etc.
+create table learn_more (
+    id number,
+    title varchar2(400),
+    description varchar2(1000),
+    link_title varchar2(400),
+    link_description varchar2(400),
+    link varchar2(400)
+); 
+
+-- Customer notifications.  This will include a notification for churn
+create table notifications (
+    cust_id number primary key,
+    notification_id number,
+    notification_date date,
+    subject varchar2(200),
+    message varchar2(4000),
+    img_url varchar2(1000),
+    type varchar2(25)
+);
+
+-- Updated movie table with ratings
+begin
+    dbms_cloud.create_external_table(
+        table_name => 'ext_movie',
+        file_uri_list => 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/adwc4pm/b/dev/o/ratings/movies.json',
+        format => json_object('skipheaders' value '0', 'delimiter' value '\n','ignoreblanklines' value 'true'),
+        column_list => 'doc varchar2(30000)'
+        );
+end;
+/
+
+create table movie as
+    select
+        cast(m.doc.movie_id as number) as movie_id,
+        cast(m.doc.title as varchar2(200 byte)) as title,   
+        cast(m.doc.budget as number) as budget,
+        cast(m.doc.gross as number) gross,
+        cast(m.doc.rating as number) rating,
+        cast(m.doc.list_price as number) as list_price,
+        cast(m.doc.genre as varchar2(4000)) as genres,
+        cast(m.doc.sku as varchar2(30 byte)) as sku,   
+        cast(m.doc.year as number) as year,
+        to_date(m.doc.opening_date, 'YYYY-MM-DD') as opening_date,
+        cast(m.doc.views as number) as views,
+        cast(m.doc.cast as varchar2(4000 byte)) as cast,
+        cast(m.doc.crew as varchar2(4000 byte)) as crew,
+        cast(m.doc.studio as varchar2(4000 byte)) as studio,
+        cast(m.doc.main_subject as varchar2(4000 byte)) as main_subject,
+        cast(m.doc.image_url as varchar2(1000)) as image_url,
+        cast(m.doc.awards as varchar2(4000 byte)) as awards,
+        cast(m.doc.nominations as varchar2(4000 byte)) as nominations,
+        cast(m.doc.runtime as number) as runtime,
+        substr(cast(m.doc.summary as varchar2(4000 byte)),1, 4000) as summary
+    from ext_movie m;
+
+alter table movie  
+add constraint genre_json_chk 
+check (genres is json) enable;
+
+/*
+    Table used for finding movies that are similar to one another:
+    "If you liked....
+ */
+
+begin
+        dbms_cloud.create_external_table(
+            table_name => 'ext_movie_similarity',
+            file_uri_list => 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/adwc4pm/b/dev/o/similar/similar-movies.json',
+            format => json_object('skipheaders' value '0', 'delimiter' value '\n','ignoreblanklines' value 'true'),
+            column_list => 'doc varchar2(30000)'
+            );
+end;
+/
+
+create table movie_similarity as
+            select
+                cast(m.doc.movie_id as number) as movie_id,
+                cast(m.doc.title as varchar2(200 byte)) as title,   
+                cast(m.doc.similar as varchar2(4000 byte)) as similar
+            from ext_movie_similarity m;
+
+/*
+ * Broken movies will be filtered and not presented in the application
+*/
+begin
+   dbms_cloud.create_external_table(
+        table_name => 'ext_broken_movies',
+        file_uri_list => 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/adwc4pm/b/dev/o/broken/broken-images.csv',
+        format => '{"delimiter":",", "ignoreblanklines":"true", "trimspaces":"lrtrim", "truncatecol":"true", "ignoremissingcolumns":"true"}',
+        column_list => 'movie_id number'
+        ); 
+end;
+/
+create table broken_movies (movie_id number);
+
+-- New movie table includes ratings
+begin
+    dbms_cloud.create_external_table(
+        table_name => 'ext_movie',
+        file_uri_list => 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/adwc4pm/b/dev/o/ratings/movies.json',
+        format => json_object('skipheaders' value '0', 'delimiter' value '\n','ignoreblanklines' value 'true'),
+        column_list => 'doc varchar2(30000)'
+        );
+end;
+/
+
+-- movie recommendations populated by graph
+create table movie_recommendations
+   (cust_id number, 
+	movie_id number, 
+	title varchar2(4000 byte), 
+	score number
+   );
+
+begin
+    workshop.write('populate demo tables');
+end;
+/
 
 /*
 1021503	Diaz	Blanca > not churning
@@ -64,15 +189,6 @@ insert into demo_customers (cust_id,img_url) values (1075252,'https://objectstor
 insert into demo_customers (cust_id,img_url) values (1010303,'https://objectstorage.us-ashburn-1.oraclecloud.com/n/adwc4pm/b/moviestream_app/o/img/avatarmale1.png');
 commit;
 
-drop table learn_more;
-create table learn_more (
-    id number,
-    title varchar2(400),
-    description varchar2(1000),
-    link_title varchar2(400),
-    link_description varchar2(400),
-    link varchar2(400)
-);
 
 insert into learn_more (
     id,
@@ -172,125 +288,14 @@ insert into learn_more (
 
 commit;
 
-
--- Add customer notifications.  This will include a notification for churn
-drop table notifications;
-create table notifications (
-    cust_id number primary key,
-    notification_id number,
-    notification_date date,
-    subject varchar2(200),
-    message varchar2(4000),
-    img_url varchar2(1000),
-    type varchar2(25)
-);
-
-
--------------------
--- 1. Recreate the movie table using one with ratings.  '
--- 2. Create the movie similarity table
--- 3. Create the movie_recommendations table
-------------------------------------------------------------------------------------------------------------------
-
-/*
-    New movie table includes ratings
- */
-drop table ext_movie;
-begin
-        dbms_cloud.create_external_table(
-            table_name => 'ext_movie',
-            file_uri_list => 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/adwc4pm/b/dev/o/ratings/movies.json',
-            format => json_object('skipheaders' value '0', 'delimiter' value '\n','ignoreblanklines' value 'true'),
-            column_list => 'doc varchar2(30000)'
-            );
-end;
-/
-
-drop table movie cascade constraints;
-create table movie as
-            select
-                cast(m.doc.movie_id as number) as movie_id,
-                cast(m.doc.title as varchar2(200 byte)) as title,   
-                cast(m.doc.budget as number) as budget,
-                cast(m.doc.gross as number) gross,
-                cast(m.doc.rating as number) rating,
-                cast(m.doc.list_price as number) as list_price,
-                cast(m.doc.genre as varchar2(4000)) as genres,
-                cast(m.doc.sku as varchar2(30 byte)) as sku,   
-                cast(m.doc.year as number) as year,
-                to_date(m.doc.opening_date, 'YYYY-MM-DD') as opening_date,
-                cast(m.doc.views as number) as views,
-                cast(m.doc.cast as varchar2(4000 byte)) as cast,
-                cast(m.doc.crew as varchar2(4000 byte)) as crew,
-                cast(m.doc.studio as varchar2(4000 byte)) as studio,
-                cast(m.doc.main_subject as varchar2(4000 byte)) as main_subject,
-                cast(m.doc.image_url as varchar2(1000)) as image_url,
-                cast(m.doc.awards as varchar2(4000 byte)) as awards,
-                cast(m.doc.nominations as varchar2(4000 byte)) as nominations,
-                cast(m.doc.runtime as number) as runtime,
-                substr(cast(m.doc.summary as varchar2(4000 byte)),1, 4000) as summary
-            from ext_movie m;
-
-alter table movie  
-add constraint genre_json_chk 
-check (genres is json) enable;
-
-/*
-    Table used for finding movies that are similar to one another:
-    "If you liked....
- */
-drop table ext_movie_similarity;
-begin
-        dbms_cloud.create_external_table(
-            table_name => 'ext_movie_similarity',
-            file_uri_list => 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/adwc4pm/b/dev/o/similar/similar-movies.json',
-            format => json_object('skipheaders' value '0', 'delimiter' value '\n','ignoreblanklines' value 'true'),
-            column_list => 'doc varchar2(30000)'
-            );
-end;
-/
-
-drop table movie_similarity cascade constraints;
-create table movie_similarity as
-            select
-                cast(m.doc.movie_id as number) as movie_id,
-                cast(m.doc.title as varchar2(200 byte)) as title,   
-                cast(m.doc.similar as varchar2(4000 byte)) as similar
-            from ext_movie_similarity m;
-
-/*
- * Broken movies will be filtered and not presented in the application
-*/
-
-drop table ext_broken_movies;
-begin
-   dbms_cloud.create_external_table(
-        table_name => 'ext_broken_movies',
-        file_uri_list => 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/adwc4pm/b/dev/o/broken/broken-images.csv',
-        format => '{"delimiter":",", "ignoreblanklines":"true", "trimspaces":"lrtrim", "truncatecol":"true", "ignoremissingcolumns":"true"}',
-        column_list => 'movie_id number'
-        ); 
-end;
-/
-
-
-create table broken_movies (movie_id number);
-
 insert into broken_movies (movie_id)
 (select movie_id from ext_broken_movies);
 commit;
 
 insert into broken_movies (movie_id)
     (select movie_id from movie where image_url is null);
-commit;    
+commit;   
 
-drop table movie_recommendations;
-create table movie_recommendations
-   (cust_id number, 
-	movie_id number, 
-	title varchar2(4000 byte), 
-	score number
-   );
 
 /*
  *  Create views that simplify JSON document creation
@@ -403,18 +408,47 @@ create or replace view v_notification_collection as
             'img_url' : img_url,
             'type' : type
     ) as doc
-    from notifications;                
+    from notifications;       
+
+/*
+    Install demo PLSQL packages
+*/
+
+declare
+    l_git varchar2(4000);
+    l_repo_name varchar2(100) := 'adb';
+    l_owner varchar2(100) := 'martygubar';
+    l_package_file varchar2(200) := 'shared/moviestream-app/sql/movieapp-package.sql';
+begin
+    workshop.write('Add demo PLSQL packages', 1);
+    -- get a handle to github
+    l_git := dbms_cloud_repo.init_github_repo(
+                repo_name       => l_repo_name,
+                owner           => l_owner );
+
+    -- install the package header
+    dbms_cloud_repo.install_file(
+        repo        => l_git,
+        file_path   => l_package_file,
+        stop_on_error => false);
+
+end;
+/
+
 
 /*
  * Update the collections that cache results
  */
+workshop.write('Create JSON collections', 1); 
 exec movieapp.initialize_collections;
+workshop.write('Add sample data', 1); 
 exec movieapp.add_sampledata;
  
 
 /*
  *  View used by OAC
  */ 
+ workshop.write('Create OAC view', 1); 
 CREATE OR REPLACE VIEW V_MOVIE_SALES AS 
   select    
     m.doc.title || '(' || m.doc.year || ')' as title,
@@ -434,36 +468,17 @@ where c.genre_id = g.genre_id
 /*
  * Open up access w/o authentication....
  */
+
+
 begin
+  workshop.write('Open access to moviestream via REST w/o authentication', 1);
   ords.delete_privilege_mapping('oracle.soda.privilege.developer', '/soda/*');
   commit;
 end;
 /
 
-
---curl -i --user B7fSMrWEa8ujI7petdSPTg..:493VFEgqeJCoHNf_Ptt8YA.. --data "grant_type=client_credentials" https://ihsf7opuc2w9rzc-db202202031659.adb.us-phoenix-1.oraclecloudapps.com/ords/moviestream/oauth/token
---curl -i --u "moviestream:bigdataPM2019%21" https://ihsf7opuc2w9rzc-db202202031659.adb.us-phoenix-1.oraclecloudapps.com/ords/moviestream/soda/latest/
-
-
-/*
--- REST access to customer 
--- moviestream-dev
---curl -X GET https://g9d1f2d8d1f91d1-moviedev.adb.us-ashburn-1.oraclecloudapps.com/ords/moviestream/soda/latest/landingPage/1265736
---curl -X GET https://g9d1f2d8d1f91d1-moviedev.adb.us-ashburn-1.oraclecloudapps.com/ords/moviestream/soda/latest/movieCollection/1216
--- adwc4pm
--- curl -X GET https://ihsf7opuc2w9rzc-db202202031659.adb.us-phoenix-1.oraclecloudapps.com/ords/moviestream/soda/latest
-
-
-
--- ADWC4PM
-mongosh 'mongodb://moviestream:bigdataPM2019%23@IHSF7OPUC2W9RZC-DB202202031659.adb.us-phoenix-1.oraclecloudapps.com:27017/moviestream?authMechanism=PLAIN&authSource=$external&ssl=true&retryWrites=false&loadBalanced=true'
-mongosh 'mongodb://moviestream:ironMan_3_pg@G9D1F2D8D1F91D1-MOVIEDEV.adb.us-ashburn-1.oraclecloudapps.com:27017/moviestream?authMechanism=PLAIN&authSource=$external&ssl=true&retryWrites=false&loadBalanced=true'
-
--- Newer clients
-mongosh 'mongodb://moviestream:ironMan_3_pg@G9D1F2D8D1F91D1-MOVIEDEV.adb.us-ashburn-1.oraclecloudapps.com:27017/moviestream?authMechanism=PLAIN&authSource=$external&ssl=true&retryWrites=false&loadBalanced=true'
--- older services (on a different port)
-mongosh 'mongodb://moviestream:ironMan_3_pg@G9D1F2D8D1F91D1-MOVIEDEV.adb.us-ashburn-1.oraclecloudapps.com:27016/moviestream?authMechanism=PLAIN&authSource=$external&ssl=true&retryWrites=false'
-
-*/
-
+BEGIN
+    workshop.write('Done.')
+end;
+/
 
